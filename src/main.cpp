fb0628167fdf48d8b83dc098109cf599376172fb
vinogradov alexey <vin.alyosha@gmail.com>
2025-01-17 16:47:55 +0300

    Переписан весь код под библиотеку GyverHub Настроены базовые команды Telegram бота

--- src/main.cpp
+++ src/main.cpp
@@ -1,261 +1,261 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 
-const char* ssidAP = "ESP-Access-Point";
-const char* password = "12345678";
+#define AP_SSID "ASUS_58_2G"
+#define AP_PASS "123qwe123"
 
-const char* PARAM_INPUT_1 = "output";
-const char* PARAM_INPUT_2 = "state";
+#define GH_INCLUDE_PORTAL 
+#include <GyverHub.h>
+GyverHub hub("MyDevices", "ESP", "");
 
-// Декларирование глобальных переменных
-String wifiIP = "";
-String actionLog[10];
-int logIndex = 0;
-const int pins[] = {21, 19, 18, 17, 16};
-unsigned long timers[5] = {0, 0, 0, 0, 0};
+#include <PairsFile.h>
+PairsFile data(&GH_FS, "/data.dat", 3000);
 
-// Декларирование функций
-AsyncWebServer server(80);
-String outputState(int output);
-String setWifi(String ssid, String pass);
-void setAP(String ssid, String pass);
-void writeToLog(String pin, String state);
+#define FB_NO_FILE
+#include <FastBot2.h>
+FastBot2 bot;
 
-// Веб-страницы, располагающиеся в памяти программ
-const char test_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Website</title>
-  </head>
-  <body>
-    <main>
-        <h1>Сейчас ваше устройство выключит точку доступа. Оно будет доступно по адресу:</h1>
-        %IPADDRESS%  
-    </main>
-  </body>
-</html>
-)rawliteral";
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta charset="utf-8">
-  <title>Реле</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
-    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
-    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-    input:checked+.slider {background-color: #b30000}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
-  </style>
-</head>
-<body>
-  <h2>ESP Реле</h2>
-  <form action="/wifi" method="post">
-    <input type="text" name="ssid" placeholder="wifi name">
-    <input type="password" name="pass" placeholder="wifi pass">
-    <button id="connect" type="submit" value="Submit">Подключиться</button>
-  </form>
-  %BUTTONPLACEHOLDER%
-  <div id="log">
-   <h3>Журнал действий</h3>
-   %LOGSPLACEHOLDER%
-  </div>
-<script>function toggleCheckbox(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1"+"&value="+element.value, true); }
-  else { xhr.open("GET", "/update?output="+element.id+"&state=0"+"&value="+element.value, true); }
-  xhr.send();
-}
-</script>
-</body>
-</html>
-)rawliteral";
+const char _ui[] PROGMEM = R"json({"type":"row","width":1,"data":[{"id":"inp","type":"input"},{"id":"sld","type":"slider"},{"id":"btn","type":"button"}]})json";
+const char* fetch_bytes = "fetch bytes\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim tellus ligula. Vivamus id lacus ac tortor commodo aliquam. Mauris eget faucibus nunc. Vestibulum tempus eu lorem a dapibus. Nullam ac dapibus ex. Aenean faucibus dapibus porttitor. Sed vel magna id tellus mattis semper. Fusce a finibus ligula. In est turpis, viverra eget libero ut, pretium pellentesque velit. Praesent ultrices elit quis facilisis mattis. Donec eu iaculis est. Sed tempus feugiat ligula non ultricies. Cras a auctor nibh, sed sodales sapien.\n\nSed cursus quam vel egestas rhoncus. Curabitur dignissim lorem sed metus sollicitudin, non faucibus erat interdum. Nunc vitae lobortis dui, mattis dignissim orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel venenatis purus. Nunc luctus leo tincidunt felis efficitur ullamcorper. Aliquam semper rhoncus odio sed porta. Quisque blandit, dui vel imperdiet ultricies, dolor arcu posuere turpis, et gravida ante libero ut ex. Vestibulum sed scelerisque nibh, nec mollis urna. Suspendisse tortor sapien, congue at aliquam vitae, venenatis placerat enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam posuere metus a est commodo finibus. Donec luctus arcu purus, sit amet sodales dolor facilisis id. Nullam consectetur sapien vitae nisi gravida, sed finibus dui hendrerit. In id pretium odio, imperdiet lacinia massa. Morbi quis condimentum ligula.";
+const char fetch_pgm[] PROGMEM = "fetch pgm\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim tellus ligula. Vivamus id lacus ac tortor commodo aliquam. Mauris eget faucibus nunc. Vestibulum tempus eu lorem a dapibus. Nullam ac dapibus ex. Aenean faucibus dapibus porttitor. Sed vel magna id tellus mattis semper. Fusce a finibus ligula. In est turpis, viverra eget libero ut, pretium pellentesque velit. Praesent ultrices elit quis facilisis mattis. Donec eu iaculis est. Sed tempus feugiat ligula non ultricies. Cras a auctor nibh, sed sodales sapien.\n\nSed cursus quam vel egestas rhoncus. Curabitur dignissim lorem sed metus sollicitudin, non faucibus erat interdum. Nunc vitae lobortis dui, mattis dignissim orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel venenatis purus. Nunc luctus leo tincidunt felis efficitur ullamcorper. Aliquam semper rhoncus odio sed porta. Quisque blandit, dui vel imperdiet ultricies, dolor arcu posuere turpis, et gravida ante libero ut ex. Vestibulum sed scelerisque nibh, nec mollis urna. Suspendisse tortor sapien, congue at aliquam vitae, venenatis placerat enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam posuere metus a est commodo finibus. Donec luctus arcu purus, sit amet sodales dolor facilisis id. Nullam consectetur sapien vitae nisi gravida, sed finibus dui hendrerit. In id pretium odio, imperdiet lacinia massa. Morbi quis condimentum ligula.";
+bool dsbl, nolbl, notab;
+String token;
 
-// Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons = "";
-    buttons += "<h4>Лампочка 1</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" value = \"1\" id=\"21\" " + outputState(21) + "><span class=\"slider\"></span></label>";
-    buttons += "<h4>Лампочка 2</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" value = \"2\" id=\"19\" " + outputState(19) + "><span class=\"slider\"></span></label>";
-    buttons += "<h4>Лампочка 3</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" value = \"3\" id=\"18\" " + outputState(18) + "><span class=\"slider\"></span></label>";
-    buttons += "<h4>Лампочка 4</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" value = \"4\" id=\"17\" " + outputState(17) + "><span class=\"slider\"></span></label>";
-    buttons += "<h4>Лампочка 5</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" value = \"5\" id=\"16\" " + outputState(16) + "><span class=\"slider\"></span></label>";
-    return buttons;
-  } else if (var == "IPADDRESS")
-  {
-    String text = "";
-    text += "<h2>"+ wifiIP +"</h2>";
-    return text;
-  } else if (var == "LOGSPLACEHOLDER")
-  {
-    String logHTML ="";
-    for (int i=0;i<10;i++){
-      if(actionLog[i] != ""){
-        logHTML += "<p><h5>" + actionLog[i] + "</h5></p>";
-      }
+// Структура релейных выходов
+struct Relay {
+    bool state;
+    int output;
+    int led;
+};
+
+Relay relay[4] = {
+    {false, 25, 21},
+    {false, 26, 19},
+    {false, 27, 18},
+    {false, 4, 17}
+};
+const int relay_count = 4;
+
+// Логгер
+gh::Log hlog;
+
+// Функция настройки выходных пинов контроллера
+void pinSetup(){
+    for (int i=0; i<relay_count; i++){
+        pinMode(relay[i].output, OUTPUT);
+        digitalWrite(relay[i].output, LOW);
+        pinMode(relay[i].led, OUTPUT);
+        digitalWrite(relay[i].led, LOW);
     }
-    return logHTML;
-  }
-  
-  
-  return String();
 }
 
-String outputState(int output){
-  if(digitalRead(output)){
-    return "checked";
-  }
-  else {
-    return "";
-  }
-}
-
-String setWifi(String ssid, String pass){
-  // Подключает к сети WiFi
+// Функция подключения к сети WiFi
+void WiFiSetup(char* ssid, char* pass){
   WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+  unsigned long start = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - start < 15000) {
+    delay(500);
+    Serial.print(".");
   }
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi connection failed");
+    return;
+  } 
+  Serial.println("WiFi connected");
   Serial.println(WiFi.localIP());
-  String out = WiFi.localIP().toString();
-  return out;
 }
 
-void setAP(String ssid, String pass){
-  // Создает локальную точку доступа
-  WiFi.softAP(ssid, pass);
-  IPAddress IP = WiFi.softAPIP();
-  Serial.println(IP);
+// Обновление состояния выходов
+void update_outputs(){
+    for (int i=0; i<relay_count; i++){
+        digitalWrite(relay[i].output, relay[i].state?HIGH:LOW);
+        digitalWrite(relay[i].led, relay[i].state?HIGH:LOW);
+        Serial.print("Relay ");
+        Serial.print(i);
+        Serial.print(" state: ");
+        Serial.println(relay[i].state);
+    }
 }
 
-void pinOutSetup(const int* pins, size_t size){
-  // Инициализация выходных пинов
-  for (size_t i = 0; i<size;i++){
-    pinMode(pins[i], OUTPUT);
-    digitalWrite(pins[i], LOW);
-  }
+// Запись токена для Telegram бота в переменную
+void set_token(gh::Build& b) {
+    token = b.value;
+    Serial.println(token);
 }
 
-void writeToLog(String pin, String state){
-  unsigned long currentTime = millis() / 1000;
-  String time = String(currentTime / 3600) + ":" + String((currentTime / 60) % 60) + ":" + String(currentTime % 60);
-  if (logIndex < 10){
-    actionLog[logIndex] =time + " - " + ((state == "1")?"Включился ":"Выключился ")+pin + " выход."; 
-    logIndex ++;
-  }
-  else{
-    for(int i=0;i<logIndex;i++){
-      actionLog[i] = actionLog[i+1];
+// Обработка входящих сообщений от Telegram бота
+void update(fb::Update& u) {
+    Text chat_id = u.message().chat().id();
+    Text message = u.message().text();
+
+    if (message == "/start") {
+        bot.sendMessage(fb::Message("Привет! Я бот для управления умным домом. Введите /menu для вызова меню управления", chat_id));
+    } else if (message == "/menu"){ 
+        fb::Message msg("Выберите номер реле, которое надо переключить", chat_id);
+        
+        fb::Menu menu;
+        menu.text = "1 ; 2 ; 3 ; 4 ";
+        menu.resize = 4;
+        menu.placeholder = "Номер выхода";
+        msg.setMenu(menu);
+
+        bot.sendMessage(msg);
+    } else {
+        int relay_index = message.toInt() - 1;
+        if (relay_index >= 0 && relay_index < 5){
+            relay[relay_index].state = !relay[relay_index].state;
+            fb::Message msg("Реле " + String(relay_index + 1) + " переключено.", chat_id);
+            bot.sendMessage(msg);
+            update_outputs();
+        }
     }
-    actionLog[logIndex] =time + " - " + ((state == "1")?"Включился ":"Выключился ")+pin + " выход."; 
-  }
 }
 
-void setup(){
-  // Serial port for debugging purposes
-  Serial.begin(115200);
-
-  
-  pinOutSetup(pins, sizeof(pins) / sizeof(pins[0]));
-  
-  // 
-  WiFi.mode(WIFI_MODE_APSTA);
-  setAP(ssidAP, password);
-
-  // Используется для отладки
-  setWifi("ASUS_58_2G", "123qwe123");
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-
-  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage1; // Номер пина
-    String inputMessage2; // Состояние
-    String inputMessage3; // Порядковый номер выхода
-    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-    if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2) && request->hasParam("value")) {
-      inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
-      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
-      inputMessage3 = request->getParam("value")->value();
-      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
-      writeToLog(inputMessage3, inputMessage2);
+// Функция построения страницы с кнопками реле
+void build_common(gh::Builder& b) {
+    b.Title("Реле");
+    {
+        gh::Row r(b);
+        b.Switch_("relay1", &relay[0].state).label("1");
+        b.Switch_("relay2", &relay[1].state).label("2");
+        b.Switch_("relay3", &relay[2].state).label("3");
+        b.Switch_("relay4", &relay[3].state).label("4");
     }
-    else {
-      inputMessage1 = "No message sent";
-      inputMessage2 = "No message sent";
+    update_outputs();
+    if (b.changed()) b.refresh();
+}
+
+// Функция построения страницы с настройками Telegram
+void build_telegram(gh::Builder& b){
+    b.Title("Подключение Telegram");
+    {
+        gh::Row r(b);
+        b.Input().size(3).label("token").attach(set_token);
+        if (b.Button().label("Сохранить").size(1).click()) {
+            data["token"] = token;
+            data.update();
+            bot.setToken(token);
+            bot.begin();
+            Serial.println("Bot started");
+            Serial.println(token);
+            bot.setPollMode(fb::Poll::Long, 20000);
+        }
     }
-    Serial.print("GPIO: ");
-    Serial.print(inputMessage1);
-    Serial.print(" - Set to: ");
-    Serial.println(inputMessage2);
-    request->send(200, "text/plain", "OK");
-  });
+}
 
-  server.on("/setTimer", HTTP_GET, [](AsyncWebServerRequest *request){
-    if (request->hasParam("value") && request->hasParam("time")){
-      int pin = request->getParam("value")->value().toInt();
-      unsigned long time = request->getParam("time")->value().toInt()*1000;
-      timers[pin] = millis() + time;
-      Serial.print("Time set for: ");
-      Serial.println(pin);
-      Serial.println(time);
+// Функция построения веб-интерфейса
+void build(gh::Builder& b) {
+    if (b.build.isSet()) hlog.println(b.build.name);
+    b.Menu(F("Реле;Telegram"));
+
+    switch (hub.menu) {
+        case 0:
+            build_common(b);
+            break;
+        case 1:
+            build_telegram(b);
+            break;
     }
-    request->send(200, "text/plain", "Timer set");
-    
-  });
+}
 
-  server.on("/wifi", HTTP_POST, [](AsyncWebServerRequest *request){
-    String input1;
-    String input2;
-    
-    if (request->hasParam("ssid", true) && request->hasParam("pass", true)){
-      input1 = request->getParam("ssid", true)->value();
-      input2 = request->getParam("pass", true)->value();
+void setup() {
+    // Инициализация
+    Serial.begin(115200);
+    hlog.begin();
+    GH_FS.begin();
+
+    pinSetup();
+    WiFiSetup(AP_SSID, AP_PASS); // Для тестов
+
+    hub.onBuild(build); //Подключение функции построения интерфейса
+
+    // Попытка настроить MQTT, пока работает только на чтение
+    hub.mqtt.config(IPAddress(192, 168, 1, 102), 1883, "emelya", "emelya");
+    hub.sendGetAuto(true);
+
+    // Обработчик консольных команд
+    hub.onCLI([](String str) {
+        Serial.println(str);
+        hub.sendCLI(str + "!");
+    });
+
+    // Настройка инфо панели
+    hub.onInfo([](gh::Info& info) {
+        switch (info.type) {
+            case gh::Info::Type::Version:
+                info.add("ESP relay interface", "v0.1");
+                break;
+            case gh::Info::Type::Network:
+                info.add(F("5G"), "50%");
+                break;
+            case gh::Info::Type::Memory:
+                info.add(F("SD"), "10 GB");
+                info.add(F("Int"), 100500);
+                break;
+            case gh::Info::Type::System:
+                info.add(F("Battery"), 3.63, 2);
+                break;
+        }
+    });
+
+    // Обработчик запросов
+    hub.onRequest([](gh::Request& req) -> bool {
+        Serial.print("Request: ");
+        Serial.print(gh::readConnection(req.client.connection()));
+        Serial.print(',');
+        Serial.print(req.client.id);
+        Serial.print(',');
+        Serial.print(gh::readCMD(req.cmd));
+        Serial.print(',');
+        Serial.print(req.name);
+        Serial.print(',');
+        Serial.print(req.value);
+        Serial.println();
+        return 1;
+    });
+
+#ifdef GH_ESP_BUILD
+    // Действия при перезагрузке
+    hub.onReboot([](gh::Reboot r) {
+        Serial.println(gh::readReboot(r));
+    });
+
+    hub.onFetch([](gh::Fetcher& f) {
+        // if (f.path == "/fetch_file.txt") f.fetchFile("/fetch_file.txt");
+        if (f.path == "/fetch_bytes.txt") f.fetchBytes((uint8_t*)fetch_bytes, strlen(fetch_bytes));
+        if (f.path == "/fetch_pgm.txt") f.fetchBytes_P((uint8_t*)fetch_pgm, strlen_P(fetch_pgm));
+    });
+
+    hub.onUpload([](String& path) {
+        Serial.print("Uploaded: ");
+        Serial.println(path);
+        if (path == "/data.dat") data.begin();  // refresh from file
+    });
+#endif
+
+    hub.begin();
+
+    bot.onUpdate(update);
+    Serial.println("Ready");
+
+    data.begin();
+    hub.setBufferSize(2000);
+
+    // Проверка наличия токена в файле, если есть - запускает бота
+    if (data.has("token")) {
+        Text raw_token = data.get("token");
+        bot.setToken(raw_token);
     }
-    else{
-      input1 = "NO MESSAGE";
-      input2 = "NO MESSAGE";
-    }
-
-    Serial.println(input1);
-    Serial.println(input2);
-    wifiIP = setWifi(input1, input2);
-    request->send_P(200, "text/html", test_html, processor);
-  });
-
-  server.on("/test", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", test_html);
-  });
-
-  // Start server
-  server.begin();
 }
 
 void loop() {
-  for (int i = 0; i < sizeof(pins) / sizeof(pins[0]); i++) {
-    if (timers[i] != 0 && millis() > timers[i]) {
-        digitalWrite(pins[i], LOW);
-        timers[i] = 0;
-        Serial.print("Timer expired for pin: ");
-        Serial.println(pins[i]);
-    }
-  }
+    data.tick();
+    hub.tick();
+    bot.tick();
 }
+
+/* TODO:
+Разобраться с MQTT, почему не работает топик set
+Добавить поле настройки для поключения к MQTT брокеру
+Добавить таймеры для реле
+Продублировать таймеры в тг боте
+*/
